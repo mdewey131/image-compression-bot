712c4f9bb145c9d658f6c0ca763814453176bd23
michael <michael.dewey131@gmail.com>
2023-03-23 20:32:04 -0700

    Following along with the rocket form example

--- compreston-site/src/main.rs
+++ compreston-site/src/main.rs
@@ -1,14 +1,40 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::{Status, ContentType};
+use rocket::form::{Form, Contextual, FromForm, FromFormField, Context};
+use rocket::fs::{FileServer, TempFile, relative};
+
+#[derive(Debug, FromForm)]
+#[allow(dead_code)]
+struct Submission<'v> {
+    #[field(validate = ext(ContentType::MP4))]
+    file: TempFile<'v>,
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", &Context::default())
+}
+#[post("/", data = "<form>")]
+fn submit<'r>(form: Form<Contextual<'r, Submit<'r>>>) -> (Status, Template) {
+    let tempalte = match form.value {
+        Some(ref submission) => {
+            println!("Submission: {:#?}", submission);
+            Template::render("success", &form.context)
+        },
+        None => {Template::render("index", &form.context)}
+    };
+
+    (form.context.status(), template)
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/hello", routes![index]);
-
+    let rocket = rocket::build()
+        .mount("/", routes![index, submit])
+        .attach(Tempalte::fairing())
+        .mount("/", FileServer::from(relative!("/static")));
+    
     Ok(rocket.into())
 }
